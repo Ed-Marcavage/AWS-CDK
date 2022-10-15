33a7d92a1cb4dca3c34f9bb6b2049acba2041c5f
Ed-Marcavage <edmarcavage256@proton.me>
2022-10-14 23:50:38 -0400

    pipeline

--- lib/aws01-stack.ts
+++ lib/aws01-stack.ts
@@ -1,26 +1,39 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
+import { Lambda } from "aws-cdk-lib/aws-ses-actions";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 import { HitCounter } from "./hitcounter";
+import { TableViewer } from "cdk-dynamo-table-viewer";
 
-export class CdkWorkshopStack extends cdk.Stack {
+// is where your CDK application’s main stack is defined. This is the file we’ll be spending most of our time in
+
+export class Aws01Stack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // defines an AWS Lambda resource
+    //constructor(scope: this, id: "HelloHandler", props?: runtime,code,handler) {
     const hello = new lambda.Function(this, "HelloHandler", {
       runtime: lambda.Runtime.NODEJS_14_X, // execution environment
+      // handler-file name, code- folder name
       code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
       handler: "hello.handler", // file is "hello", function is "handler"
     });
 
     const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
+      // plugs into- downstream: lambda.IFunction; in hitcounter.ts
+      // conects to const hello ^
       downstream: hello,
     });
 
     // defines an API Gateway REST API resource backed by our "hello" function.
     new apigw.LambdaRestApi(this, "Endpoint", {
+      // conects to const helloWithCounter ^
       handler: helloWithCounter.handler,
+    });
+
+    new TableViewer(this, "ViewHitCounter", {
+      title: "Hello Hits",
+      table: helloWithCounter.table,
     });
   }
 }
--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -5,22 +5,46 @@ import { Construct } from "constructs";
 
 export interface HitCounterProps {
   /** the function for which we want to count url hits **/
+  // downstream: hello, in /lib plugs in hello lambda here
   downstream: lambda.IFunction;
+  /**
+   * The read capacity units for the table
+   *
+   * Must be greater than 5 and lower than 20
+   *
+   * @default 5
+   */
+  readCapacity?: number;
 }
 
 export class HitCounter extends Construct {
   /** allows accessing the counter function */
   public readonly handler: lambda.Function;
 
+  /** the hit counter table */
+  public readonly table: dynamodb.Table;
+
   constructor(scope: Construct, id: string, props: HitCounterProps) {
+    if (
+      props.readCapacity !== undefined &&
+      (props.readCapacity < 5 || props.readCapacity > 20)
+    ) {
+      throw new Error("readCapacity must be greater than 5 and less than 20");
+    }
     super(scope, id);
 
     const table = new dynamodb.Table(this, "Hits", {
       partitionKey: { name: "path", type: dynamodb.AttributeType.STRING },
+      encryption: dynamodb.TableEncryption.AWS_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      readCapacity: props.readCapacity ?? 5,
     });
+
+    this.table = table;
 
     this.handler = new lambda.Function(this, "HitCounterHandler", {
       runtime: lambda.Runtime.NODEJS_14_X,
+      // handler-file name, code- folder name
       handler: "hitcounter.handler",
       code: lambda.Code.fromAsset("lambda"),
       environment: {
@@ -29,7 +53,12 @@ export class HitCounter extends Construct {
       },
     });
 
+    // dynamodb:UpdateItem on resource: arn:aws:dynamodb:us-east-1:647966781979:table/Aws01Stack-HelloHitCounterHits7AAEBF80-HCT9QJPRES9Y because no identity-based policy allows the dynamodb:UpdateItem action
+    // grant the lambda role read/write permissions to our table to avoid error above
     table.grantReadWriteData(this.handler);
+
+    //not authorized to perform: lambda:InvokeFunction on resource: arn:aws:lambda:us-east-1:647966781979:function:Aws01Stack-HelloHandler2E4FBA4D-bNwBlXyHouiV because no identity-based policy allows the lambda:InvokeFunction action"
+    // grant the lambda role invoke permissions to the downstream function
     props.downstream.grantInvoke(this.handler);
   }
 }
